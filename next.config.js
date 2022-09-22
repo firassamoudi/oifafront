759757ba34668f6ca781012abb118e6ff3d21414
firassamoudi <36210857+firassamoudi@users.noreply.github.com>
2022-09-22 23:56:46 +0100

    2

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  // - Development
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...defaultConfig,
+      env: {
+      //  APP_URL: "http://dev.oifa.website:3000",
+      //  API_URL: "http://dev.oifa.api:3001",
+        APP_URL: "http://localhost:3002/",
+        API_URL: "http://dev.oifa.api:3001",
+      },
+    };
+  }
+
+  // - Production
+  return {
+    ...defaultConfig,
+    env: {
+      APP_URL: "https://beta.oifa.tech",
+      API_URL: "https://app.oifa.tech",
+    },
+  };
+};
